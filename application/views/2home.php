d593220767cafe5c13943511b4c32c84b0c171ff
Bioniko <85115922+Bioniko@users.noreply.github.com>
2024-01-22 09:12:15 -0600

    Update 2home.php

--- application/views/2home.php
+++ application/views/2home.php
@@ -1,19 +1,43 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <style>
+    @media (min-width: 300px) and (max-width: 500px) {
+      .tamanio{
+        width: 45%!important;
+      }
+      .bajar{
+        padding-top: 5%;
+      }
+      .tamaniologo{
+        width:100% !important;
+      }
+    }
+    .tamanio{
+      width: 20%;
+    }
+    .bajar{
+      padding-top: 8%;
+    }
+    .tamaniologo{
+      width:70%;
+      height: 30vh;
+    }
+  </style>
 <title>TeguClub</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link href="<?php echo base_url();?>Tema/NiceAdmin/assets/img/favicon.png" rel="icon">
 <link href="<?php echo base_url();?>Tema/NiceAdmin/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
 
 <!-- Navbar (sit on top) -->
 <div class="w3-top">
   <div class="w3-bar w3-white w3-wide w3-padding w3-card" style="text-align: center">
-    <img src="<?php echo base_url();?>estilos/images/teguclub.png" style="width: 40%">
+    <img src="<?php echo base_url();?>estilos/images/teguclub.png" class="tamanio">
     <!-- Float links to the right. Hide them on small screens -->
    
   </div>
@@ -26,7 +50,7 @@
     
   </div>
 </header>
-
+<div class="bajar"></div>
 <!-- Page content -->
 <div class="w3-content w3-padding" style="max-width:1564px">
 
@@ -38,27 +62,29 @@
 
   
 
- 
+ <div class="row">
   <?php
   foreach ($emp as $emp) {
     if($emp){
       if($emp->emp_id != 2){
   ?>
-  <a class="w3-row-padding " href="<?php echo base_url();?>index.php/Dindex/Show?emp=<?php echo $emp->emp_id;?>">
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <img src="<?php echo base_url();?>assets/uploads/files/<?php echo $emp->emp_logo; ?>" alt="John" style="width:100%">
-      <h3><?php echo $emp->emp_nombre;?></h3>
-      <p class="w3-opacity"></p>
-      <p><?php echo $emp->emp_descripcion; ?></p>
-      <p><button class="w3-button w3-light-grey w3-block">Ingresar</button></p>
-    </div>
+  <div class="col-sm-6 col-md-3 col-lg-3">
+    <a class="w3-row-padding " href="<?php echo base_url();?>index.php/Dindex/Show?emp=<?php echo $emp->emp_id;?>">
+      <div class="">
+        <img src="<?php echo base_url();?>assets/uploads/files/<?php echo $emp->emp_logo; ?>" alt="John" class="tamaniologo">
+        <h3><?php echo $emp->emp_nombre;?></h3>
+        <p class="w3-opacity"></p>
+        <p><?php echo $emp->emp_descripcion; ?></p>
+        <p><button class="w3-button w3-light-grey w3-block">Ingresar</button></p>
+      </div>
     </a>
+  </div>
   <?php
       }
     }
   }
   ?>
-  
+  </div>
 
 
 <!-- End page content -->
